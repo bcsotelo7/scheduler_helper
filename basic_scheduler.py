475b8722dfc169c6a4b4ed732b312269a9c5894e
Bruno Sotelo <sotelobc@Brunos-MBP-2.attlocal.net>
2025-08-04 00:46:52 -0700

    validation

--- basic_scheduler.py
+++ basic_scheduler.py
@@ -7,12 +7,13 @@ import ast
 
 def parser():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-i','--input', help="Specify input file (tasks)", required=True)
+    parser.add_argument("-i","--input", help="Specify input file (tasks)", required=True)
+    parser.add_argument("--validate", help="Validate file only", required=False, action="store_true")
     return parser.parse_args()
 
 def validate_file(filename):
     if not os.path.exists(filename):
-        print(f'Error: File {filename} not found.')
+        print(f"Error: File {filename} not found.")
         sys.exit(1)
     else:
         print(f"Found {filename}.")
@@ -25,24 +26,27 @@ def load_file(valid_filename):
             print(line)
             name, duration, deps = line.strip().split(',')
             tasks.append({
-                'name': name,  # string
-                'duration': int(duration),  # integer
-                'dependencies': ast.literal_eval(deps.strip())  # list of names
+                "name": name,  # string
+                "duration": int(duration),  # integer
+                "dependencies": ast.literal_eval(deps.strip())  # list of names
             })
+    print("This is a valid tasks file")
     return tasks
 
 def serial_mode(tasks_list):
-    serial_duration = sum(i['duration'] for i in tasks_list)
+    serial_duration = sum(i["duration"] for i in tasks_list)
     return serial_duration
 
 def main():
     args = parser()
     filename = args.input
-    print(f'File entered: {filename}')
+    print(f"File entered: {filename}")
     valid_filename = validate_file(filename)
     tasks_list = load_file(valid_filename)
     serial_duration = serial_mode(tasks_list)
     print(f"Estimated total runtime (no dependencies considered): {serial_duration} seconds")
+    if args.validate:
+        sys.exit(0)
 
 
 if __name__ == "__main__":
